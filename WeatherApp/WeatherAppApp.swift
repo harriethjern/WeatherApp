1ddc87896b6abd80cb9a33c0ae0987e1fa14ebfb
Harriet <hjernharriet84@gmail.com>
2025-02-05 12:29:06 +0100

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Harriet Hjern on 2025-02-05.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
